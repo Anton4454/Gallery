0d95964411b99e3955ec88fbb3f0fb4cfde60974
azyuzelev <azyuzelev@gmail.com>
2020-07-02 18:03:41 +0300

    splash

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 2500);
+    }
+}
